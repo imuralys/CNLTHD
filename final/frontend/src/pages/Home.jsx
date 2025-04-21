6c3db029af6a83f28a3c0ea5ce2afc865bec96b1
Alys <nnhn2953@gmail.com>
2025-04-21 13:14:01 +0700

    update

--- final/frontend/src/pages/Home.jsx
+++ final/frontend/src/pages/Home.jsx
@@ -26,9 +26,9 @@ function Home() {
                                 backgroundImage: `url(https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)`
                             }}
                         >
-                            <div className="absolute inset-0 bg-gradient-to-r from-purple-900/70 to-blue-900/70 flex items-center justify-center">
+                            <div className="absolute inset-0 bg-gradient-to-r from-purple-900/30 to-blue-900/30 flex items-center justify-center">
                                 <div className="text-center text-white">
-                                <h1 className="text-3xl md:text-5xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-400" style={{ fontFamily: "'Orbitron', sans-serif", letterSpacing: '2px' }}>GAMIFY STORE</h1>
+                                <h1 className="text-3xl md:text-5xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-blue-400" style={{ fontFamily: "'Orbitron', sans-serif", letterSpacing: '2px' }}>GAMIFY STORE</h1>
                                     <p className="text-lg md:text-xl mb-6">Thế giới game đích thực dành cho bạn</p>
                                 </div>
                             </div>
